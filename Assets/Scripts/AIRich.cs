eed788df2e990053df9d0b96431dda54e86fa71f
Solobolt <solobolt@gmail.com>
2016-04-27 21:08:04 +1000

    started AIRich slime script

---
+++ Assets/Scripts/AIRich.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIRich : MonoBehaviour {
+
+    //public AIManager aiManager;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// if player is # close then run to x
+    /// </summary>
+    void RichActions()
+    {
+
+    }
+}
