f5f8f13b01f6187ec168bfc19f39298932c8464c
opke11 <e.deeth@hotmail.com>
2016-04-28 01:18:00 +1000

    Added Hugging Script

---
+++ Assets/Scripts/Hugging.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Hugging : MonoBehaviour {
+
+    public AnimationCurve SlimeFrequencyAndAmplitude;
+    public Slider SlimeIcon;
+
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        SlimeFrequencyAndAmplitude = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
+        SlimeFrequencyAndAmplitude.preWrapMode = WrapMode.Loop;
+        SlimeFrequencyAndAmplitude.postWrapMode = WrapMode.Loop;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+              SlimeIcon.value = SlimeFrequencyAndAmplitude.Evaluate(Time.time);
+    }
+}
